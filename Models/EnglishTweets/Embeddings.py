059f2933c35645234c44f95ecbd1aa239c8a6f31
AbirJAMALY <116753136+AbirJAMALY@users.noreply.github.com>
2024-04-19 18:29:56 -0400

    Update Embeddings.py

--- Models/EnglishTweets/Embeddings.py
+++ Models/EnglishTweets/Embeddings.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import accuracy_score, f1_score
 from sklearn.model_selection import train_test_split
-from transformers import BertTokenizer
+from transformers import BertTokenizer, BertModel
 from torch import nn
 from transformers import Trainer,TrainingArguments
 from transformers import AutoModelForSequenceClassification
@@ -11,21 +11,45 @@ from transformers import AutoTokenizer
 from transformers import AutoModel, DebertaV2Tokenizer
 
 
-
-
 class Embeddings():
    def __init__(self) :
         pass
-   
+  
    def bert(self, train_tweets, val_tweets, test_tweets):
-      tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', num_labels=2,
+        # Initialize the BERT tokenizer
+        tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', num_labels=2,
                                                 loss_function_params={"weight": [0.75, 0.25]})
+        
+        # Tokenize the input tweets and pad/truncate them to a maximum length of 512 tokens
+        train_encodings = tokenizer(train_tweets, padding=True, truncation=True, max_length=512)
+        val_encodings = tokenizer(val_tweets, padding=True, truncation=True, max_length=512)
+        test_encodings = tokenizer(test_tweets, padding=True, truncation=True, max_length=512)
 
-      train_encodings = tokenizer(train_tweets, padding=True, truncation=True, max_length=512)
-      val_encodings = tokenizer(val_tweets, padding=True, truncation=True, max_length=512)
-      test_encodings = tokenizer(test_tweets, padding=True, truncation=True, max_length=512)
 
-      return tokenizer, train_encodings, val_encodings, test_encodings
+        # Convert the encodings to tensors
+        input_ids_train = torch.tensor(train_encodings['input_ids'])
+        attention_mask_train = torch.tensor(train_encodings['attention_mask'])
+        input_ids_val = torch.tensor(val_encodings['input_ids'])
+        attention_mask_val = torch.tensor(val_encodings['attention_mask'])
+        input_ids_test = torch.tensor(test_encodings['input_ids'])
+        attention_mask_test = torch.tensor(test_encodings['attention_mask'])
+
+        # Load the pre-trained BERT model
+        model = BertModel.from_pretrained('bert-base-uncased')
+
+        # Pass the tokenized input through the model
+        with torch.no_grad():
+            outputs_train = model(input_ids=input_ids_train, attention_mask=attention_mask_train)
+            outputs_val = model(input_ids=input_ids_val, attention_mask=attention_mask_val)
+            outputs_test = model(input_ids=input_ids_test, attention_mask=attention_mask_test)
+
+        # Extract the embeddings from the model's output
+        embeddings_train = outputs_train.last_hidden_state
+        embeddings_val = outputs_val.last_hidden_state
+        embeddings_test = outputs_test.last_hidden_state
+
+        return tokenizer, embeddings_train, embeddings_val, embeddings_test
+   
 
    def create_dataset(self, encodings, labels):
       """
@@ -37,17 +61,41 @@ class Embeddings():
       })
       return dataset
       
-
    def roberta(self, train_tweets, val_tweets, test_tweets):
-      task='sentiment'
-      MODEL = f"cardiffnlp/twitter-roberta-base-{task}"
-      tokenizer = AutoTokenizer.from_pretrained(MODEL, num_labels=2, loss_function_params={"weight": [0.75, 0.25]})
+        task = 'sentiment'
+        MODEL = f"cardiffnlp/twitter-roberta-base-{task}"
+        
+        # Initialize the RoBERTa tokenizer
+        tokenizer = AutoTokenizer.from_pretrained(MODEL)
+        
+        # Tokenize the input tweets and pad/truncate them to a maximum length of 512 tokens
+        train_encodings = tokenizer(train_tweets, padding=True, truncation=True, max_length=512)
+        val_encodings = tokenizer(val_tweets, padding=True, truncation=True, max_length=512)
+        test_encodings = tokenizer(test_tweets, padding=True, truncation=True, max_length=512)
 
-      train_encodings = tokenizer(train_tweets, padding=True, truncation=True, max_length=512)
-      val_encodings = tokenizer(val_tweets, padding=True, truncation=True, max_length=512)
-      test_encodings = tokenizer(test_tweets, padding=True, truncation=True, max_length=512)
+        # Convert the encodings to tensors
+        input_ids_train = torch.tensor(train_encodings['input_ids'])
+        attention_mask_train = torch.tensor(train_encodings['attention_mask'])
+        input_ids_val = torch.tensor(val_encodings['input_ids'])
+        attention_mask_val = torch.tensor(val_encodings['attention_mask'])
+        input_ids_test = torch.tensor(test_encodings['input_ids'])
+        attention_mask_test = torch.tensor(test_encodings['attention_mask'])
 
-      return tokenizer, train_encodings, val_encodings, test_encodings
+        # Load the pre-trained RoBERTa model
+        model = AutoModelForSequenceClassification.from_pretrained(MODEL)
+
+        # Pass the tokenized input through the model
+        with torch.no_grad():
+            outputs_train = model(input_ids=input_ids_train, attention_mask=attention_mask_train)
+            outputs_val = model(input_ids=input_ids_val, attention_mask=attention_mask_val)
+            outputs_test = model(input_ids=input_ids_test, attention_mask=attention_mask_test)
+
+        # Extract the embeddings from the model's output
+        embeddings_train = outputs_train.last_hidden_state
+        embeddings_val = outputs_val.last_hidden_state
+        embeddings_test = outputs_test.last_hidden_state
+
+        return tokenizer, embeddings_train, embeddings_val, embeddings_test
 
 
    def create_dataframe(self, embeddings, labels):
@@ -63,25 +111,36 @@ class Embeddings():
       # Créer le DataFrame à partir de la liste de dictionnaires
       df = pd.DataFrame(df)
       return df
-   
+  
 
    def deberta(self, train_tweets, val_tweets, test_tweets):
-      
-      model = AutoModel.from_pretrained("microsoft/DeBERTa-V3-XSmall")
-      tokenizer = DebertaV2Tokenizer.from_pretrained("microsoft/DeBERTa-V3-XSmall")
+        # Charger le modèle et le tokenizer DeBERTa
+        model = AutoModel.from_pretrained("microsoft/DeBERTa-V3-XSmall")
+        tokenizer = DebertaV2Tokenizer.from_pretrained("microsoft/DeBERTa-V3-XSmall")
 
-      train_encodings = tokenizer(train_tweets, padding=True, truncation=True, max_length=512)
-      val_encodings = tokenizer(val_tweets, padding=True, truncation=True, max_length=512)
-      test_encodings = tokenizer(test_tweets, padding=True, truncation=True, max_length=512)
+        # Tokenizer les tweets d'entraînement, de validation et de test
+        train_encodings = tokenizer(train_tweets, padding=True, truncation=True, max_length=512)
+        val_encodings = tokenizer(val_tweets, padding=True, truncation=True, max_length=512)
+        test_encodings = tokenizer(test_tweets, padding=True, truncation=True, max_length=512)
 
-      return tokenizer, train_encodings, val_encodings, test_encodings
-   
+        # Convertir les encodages en tensors
+        input_ids_train = torch.tensor(train_encodings['input_ids'])
+        attention_mask_train = torch.tensor(train_encodings['attention_mask'])
+        input_ids_val = torch.tensor(val_encodings['input_ids'])
+        attention_mask_val = torch.tensor(val_encodings['attention_mask'])
+        input_ids_test = torch.tensor(test_encodings['input_ids'])
+        attention_mask_test = torch.tensor(test_encodings['attention_mask'])
 
+        # Passer les entrées tokenisées à travers le modèle
+        with torch.no_grad():
+            outputs_train = model(input_ids=input_ids_train, attention_mask=attention_mask_train)
+            outputs_val = model(input_ids=input_ids_val, attention_mask=attention_mask_val)
+            outputs_test = model(input_ids=input_ids_test, attention_mask=attention_mask_test)
 
+        # Extraire les embeddings à partir de la dernière couche cachée du modèle
+        embeddings_train = outputs_train.last_hidden_state
+        embeddings_val = outputs_val.last_hidden_state
+        embeddings_test = outputs_test.last_hidden_state
 
-
-
-   
-   
-
+        return tokenizer, embeddings_train, embeddings_val, embeddings_test 
 
